cecbe1011b2405b53de38423938d8ab8fb905420
ValeriaMalin <156071562+ValeriaMalin@users.noreply.github.com>
2025-01-15 10:41:38 +0100

    Update windpark_app.py

--- windpark_app.py
+++ windpark_app.py
@@ -75,19 +75,19 @@ st.markdown("""
 
 /* Style the main page title */
 h1 {
-    font-size: 14px !important; /* Adjust title font size */
+    font-size: 20px !important; /* Adjust title font size */
     color: white; /* Change title color */
 }
 
 /* Style subheaders */
 h2 {
-    font-size: 12px !important; /* Adjust subheader font size */
+    font-size: 16px !important; /* Adjust subheader font size */
     color: white; /* Change subheader color */
 }
 
 /* Style smaller headers */
 h3 {
-    font-size: 10px !important; /* Adjust smaller header font size */
+    font-size: 14px !important; /* Adjust smaller header font size */
     color: white; /* Change smaller header color */
 }   
 
@@ -105,12 +105,12 @@ h1, h2, h3, h4, h5, h6, p, span, div {
 /* Expander header and content */
 .streamlit-expanderHeader {
     color: white !important;
-    font-size: 10px !important;
+    font-size: 14px !important;
 }
 
 .streamlit-expanderContent {
     color: white !important;
-    font-size: 10px !important;
+    font-size: 14px !important;
 }
 </style>
 """,
@@ -450,7 +450,7 @@ These turbines represent the pinnacle of wind energy technology, offering flexib
 
      # Customize the layout to match the app style
      fig5.update_layout(
-       width=700, height=400,
+       width=700, height=500,
        barmode='stack', 
        xaxis_tickangle=-45,  # Rotate x-axis labels
        xaxis=dict(
